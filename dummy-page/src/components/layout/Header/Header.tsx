818005de69f6dedbf541d9a1ec61a1a60cb9037c
Quy <thienquy236@gmail.com>
2026-07-08 00:07:50 +0700

    Update first Layout

---
+++ dummy-page/src/components/layout/Header/Header.tsx
@@ -0,0 +1,13 @@
+import Layout, { Header } from "antd/es/layout/layout";
+
+const HeaderLayout= () => {
+  return (
+    <Layout>
+        <Header className="Mainpage-header">
+          Header
+        </Header>
+    </Layout>
+  );
+};
+
+export default HeaderLayout;
--- dummy-page/src/page/Mainpage/Mainpage.tsx
+++ dummy-page/src/page/Mainpage/Mainpage.tsx
@@ -1,9 +1,19 @@
+import { Layout } from "antd";
+import HeaderLayout from "../../components/layout/Header/Header";
+import FooterLayout from "../../components/layout/Footer/Footer";
+import Sider from "antd/es/layout/Sider";
+
 const Mainpage = () => {
   return (
-    <div className="Mainpage">
-        <Sidebar />
+    <div className="Mainpage-wrapper">
+      <Layout hasSider className="Mainpage-layout">
+        <Sider></Sider>
+      </Layout>
+      <HeaderLayout />
+      <Layout className="Mainpage-content">Content</Layout>
+      <FooterLayout />
     </div>
-  )
-}
+  );
+};
 
-export default Mainpage
+export default Mainpage;
