642c2aff140f2d7fbe63e538caa9e97e28147a26
QuyThien <thienquy236@gmail.com>
2026-07-15 13:26:35 +0700

    update redux Frame

---
+++ dummy-page/src/store/root-reducer.ts
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+
+
+export const rootReducer = combineReducers({
+  
+});
+
+
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
--- dummy-page/src/store/store.ts
+++ dummy-page/src/store/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { rootReducer } from './root-reducer'
 
 export const store = configureStore({
-  reducer: {
-    // slices will go here later, e.g. counter: counterReducer
-  },
+  reducer: rootReducer
+
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = () => store.dispatch
+
+export const useAppSelector: <TSelected>(selector: (state: RootState) => TSelected) => TSelected = (selector) => {
+  return selector(store.getState())
+}
